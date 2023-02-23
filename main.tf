3b628c4c6c90036b4e76aa5480f7ba3152e98fd8
ev <mtndrg@gmail.comgit config --global user.email mtndrg@gmail.com>
2023-02-24 00:51:37 +0300

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+#   profile = "mtn"
+}
+
+resource "aws_s3_bucket" "myBucket" {
+    bucket = var.name
+}
